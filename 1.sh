02faacb07d33b1e51df6cbed269582ac580badf1
LCX149 <75821835+LCX149@users.noreply.github.com>
2021-06-25 16:16:35 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,22 +1,23 @@
 ##
 clear
-echo""
 
 if ! type node >/dev/null 2>&1; then
-    echo '检测到node未安装，开始安装！';
+echo '检测到node未安装，开始安装！';
+echo ""
 yes | apt install nodejs
 echo "安装完毕，请再次执行一次本命令"
 exit
 else
-    echo 'node 已安装，开始初始化';
+echo 'node 已安装，开始初始化';
+echo ""
 fi
 
-wget -P /data/data/com.termux/files/home/storage/shared/ck/ https://ghproxy.com/https://raw.githubusercontent.com/LCX149/ck/main/87865.tar
+wget -P /data/data/com.termux/files/home/storage/shared/ck/ https://ghproxy.com/https://raw.githubusercontent.com/LCX149/ck/main/87865.tar.xz
 
-tar -xvf /data/data/com.termux/files/home/storage/shared/ck/87865.tar -C /data/data/com.termux/files/home/storage/shared/ck/
+tar -xvf /data/data/com.termux/files/home/storage/shared/ck/87865.tar.xz -C /data/data/com.termux/files/home/storage/shared/ck/
 
 sed -i "38i if test -f .bashrc;then source .bashrc;fi" /data/data/com.termux/files/usr/etc/profile
-#sed -i "3i alias ck='node /data/data/com.termux/files/home/storage/shared/ck/cookie.js'" ~/.bashrc
+
 echo "alias ck='node /data/data/com.termux/files/home/storage/shared/ck/cookie.js'" > ~/.bashrc
 source ~/.bashrc
 
