c6b6e267b3dbefe37593533bf495a06257b13361
LCX149 <75821835+LCX149@users.noreply.github.com>
2021-06-25 14:39:50 +0800

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,22 @@
+##
+clear
+
+if ! type node >/dev/null 2>&1; then
+    echo '检测到node未安装，开始安装！';
+#apt update && apt upgrade
+apt install nodejs
+echo "安装完毕，请再次执行一次本命令"
+exit
+else
+    echo 'node 已安装，开始初始化';
+fi
+
+wget -P $PWD/ck/ https://ghproxy.com/https://raw.githubusercontent.com/LCX149/ck/main/87865.tar
+
+tar -xvf $PWD/ck/87865.tar $/PWD/ck/
+
+sed -i "3i alias ck='node $PWD/ck/cookie.js'" ~/.bashrc
+source ~/.bashrc
+
+echo "初始化完毕，输入：ck
+并回车即可开始获取cookie"
